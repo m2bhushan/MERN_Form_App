d3a03408b6e6a9978d4f5b990fd216ad24f07575
m2bhushan <67832647+m2bhushan@users.noreply.github.com>
2023-12-20 12:53:08 +0530

    Update App.js

--- mernstackform/src/App.js
+++ mernstackform/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [form, setForm] = useState({});
+  const [users, setUsers] = useState([]);
+
+
+  const handleForm= (e)=>{
+// console.log(e.target.value, e.target.name);
+setForm({
+  ...form,
+  [e.target.name] : e.target.value
+})
+}
+
+const handleSubmit= async (e) => {
+  // e.preventDefault();
+
+  const response = await fetch("http://localhost:8080/demo",{
+    // method:'POST'
+    method:'POST',
+    body: JSON.stringify(form),
+    headers:{
+      'Content-Type' : 'application/json'
+    }
+  })
+  const data= await response.json();
+  console.log(data);
+}
+
+const getUsers = async () =>{
+  const response= await fetch("http://localhost:8080/demo",{
+    method:'GET',
+  })
+  const data= await response.json();
+  setUsers(data);
+}
+
+useEffect(()=>{
+  getUsers();
+},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit} className="form" >
+      
+        <span>Username :</span>
+        <input type="text" placeholder="username" name="username" className="input" onChange={handleForm}></input><br></br><br></br>
+        <span>Password :</span>&nbsp;
+
+        <input type="text" placeholder="password" name="password" className="input" onChange={handleForm}></input><br></br><br></br>
+
+        <button className="btn">Submit</button>
+
+        <p className='output'>{JSON.stringify(form)}</p>
+      </form>
+
+      <div className='output2'>
+       {/* <div> */}
+        <ul>
+         {users.map(user=> <li>{user.username},{user.password}</li>)}
+        </ul>
+      </div>
+      
     </div>
   );
 }
